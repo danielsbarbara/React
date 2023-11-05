9cbff5dc497557e416a359f4ef03fb0bb1e3323f
Daniel Bárbara <daniel.santos.barbara@gmail.com>
2023-11-05 00:43:36 +0000

    popup added

--- encrypted-login/components/Button/Button.jsx
+++ encrypted-login/components/Button/Button.jsx
@@ -2,21 +2,25 @@ import { login } from "@/frontendlogic/login"
 import styles from "./Button.module.css"
 import { useRouter } from "next/navigation"
 import { signin } from "../../frontendlogic/signin"
+import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export function Button({ page, info }) {
     const router = useRouter()
-
+    const notifyS = () => toast.success("Registo criado com sucesso")
+    const notifyD = () => toast.error("Dados inválidos")
     async function onClick() {
 
         if (page === 'login') {
             const result = await login(info)
-            if (!result) return 
+            if (!result) return notifyD()
+            notifyS()
             localStorage.setItem('token', JSON.stringify(result.result))
         } else {
             const result = await signin(info)
-            if(!result) return
+            if (!result) return notifyD()
+            notifyS()
             setTimeout(() => {
                 localStorage.setItem('token', JSON.stringify(result.result))
             }, 1000)
@@ -29,6 +33,18 @@ export function Button({ page, info }) {
         <div className={styles.container}>
             <button className={styles.button} onClick={onClick}>{page === 'login' ? 'Entrar' : 'Submeter'}</button>
             {page === 'login' && <button className={styles.button} onClick={() => gotoRegisterPage()}>Registar</button>}
+            <ToastContainer
+                position="top-center"
+                autoClose={2000}
+                hideProgressBar={true}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </div>
     )
 }
--- encrypted-login/frontendlogic/signin.js
+++ encrypted-login/frontendlogic/signin.js
@@ -1,30 +1,27 @@
-export async function signin(info){
-    if(!info.name || !info.email || !info.password) return
+export async function signin(info) {
+  if (!info.name || !info.email || !info.password) return false
 
-    const options = {
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', 'User-Agent': 'insomnia/8.3.0' },
+    body: JSON.stringify({ name: info.name, email: info.email, password: info.password })
+  };
+
+  const res = await fetch('/api/signin', options)
+  if (res.status === 200) {
+    const result = await res.json()
+    if (result) {
+      const options = {
         method: 'POST',
-        headers: {'Content-Type': 'application/json', 'User-Agent': 'insomnia/8.3.0'},
-        body: JSON.stringify({name: info.name, email: info.email, password: info.password})
-      };
-
-      const res = await fetch('/api/signin', options)
-      if(res.status === 200){
-        const result = await res.json()
-        if(result){
-            const options = {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json', 'User-Agent': 'insomnia/8.3.0'},
-                body: JSON.stringify({email: info.email})
-            }
-            const resToken = await fetch('/api/gettoken', options)
-            if(resToken.status === 200){
-                const token = await resToken.json()
-                return token
-            }
-        } else {
-            return false
-        }
-      } else {
-        return false
+        headers: { 'Content-Type': 'application/json', 'User-Agent': 'insomnia/8.3.0' },
+        body: JSON.stringify({ email: info.email })
       }
+      const resToken = await fetch('/api/gettoken', options)
+      if (resToken.status === 200) {
+        const token = await resToken.json()
+        return token
+      }
+    }
+  }
+  return false
 }
