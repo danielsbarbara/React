5fe14e3b50194b5ea9badcd8be5916960fe4d8d1
Daniel Bárbara <daniel.santos.barbara@gmail.com>
2023-11-05 21:46:41 +0000

    .

--- encrypted-login/pages/api/gettoken.js
+++ encrypted-login/pages/api/gettoken.js
@@ -1,7 +1,9 @@
 import { gettoken } from "@/backendLogic/gettokens"
 
 export default async (req, res) => {
-    const {email} = req.body
-    const token = await gettoken(email)
-    res.status(200).json({result: token})
+    if(req.method === 'POST'){
+        const {email} = req.body
+        const token = await gettoken(email)
+        res.status(200).json({result: token})
+    }
 }
