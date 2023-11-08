7ac286437c831970ceea6f84994e7d912b6931ff
Daniel Bárbara <daniel.santos.barbara@gmail.com>
2023-11-08 18:35:19 +0000

    ajust url

--- SpotiStats/database/mongoconnection.js
+++ SpotiStats/database/mongoconnection.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb')
 // const url = "mongodb://127.0.0.1:27017"
-const url = "mongodb+srv://danielsantosbarbara:bLvMHsQFTeCceFAp@cluster0.hu9a4ao.mongodb.net/?retryWrites=true&w=majority"
+const url = process.env.URL_DATABASE
 const defaultDbName = "SpotiStats"
 let client = undefined
 
--- encrypted-login/pages/api/gettoken.js
+++ encrypted-login/pages/api/gettoken.js
@@ -1,9 +1,7 @@
 import { gettoken } from "@/backendLogic/gettokens"
 
 export default async (req, res) => {
-    if(req.method === 'POST'){
         const {email} = req.body
         const token = await gettoken(email)
         res.status(200).json({result: token})
-    }
 }
