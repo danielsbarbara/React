00d6ee788006ad2421667bd95c84142f32b59230
Daniel Bárbara <daniel.santos.barbara@gmail.com>
2023-11-08 19:20:49 +0000

    .

--- SpotiStats/database/mongoconnection.js
+++ SpotiStats/database/mongoconnection.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb')
 // const url = "mongodb://127.0.0.1:27017"
-const url = process.env.URL_DATABASE
+const url = process.env.MONGODB_URI
 const defaultDbName = "SpotiStats"
 let client = undefined
 
