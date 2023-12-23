4b9c6645ad57b420463e73ea85170381a87a8f7c
Daniel Bárbara <daniel.santos.barbara@gmail.com>
2023-12-23 23:28:44 +0000

    .

--- eagles-on-the-road/logic/frontend/fetchJWTToken.ts
+++ eagles-on-the-road/logic/frontend/fetchJWTToken.ts
@@ -3,7 +3,7 @@ export async function validateToken(jwt: any) {
         method: 'GET',
         headers: {'Authorization': `jwt ${jwt}`}
     }
-    const res = await fetch('api/v1/token', options)
+    const res = await fetch('/api/v1/token', options)
     if(res.status === 200){
         return true
     }
