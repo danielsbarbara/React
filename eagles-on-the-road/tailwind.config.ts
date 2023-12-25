09230bbc0fada90c33fe56ae7d9e1b50cac0fb0f
Daniel Bárbara <daniel.santos.barbara@gmail.com>
2023-12-25 15:27:09 +0000

    .

--- eagles-on-the-road/tailwind.config.ts
+++ eagles-on-the-road/tailwind.config.ts
@@ -2,8 +2,8 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './Pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './Components/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
