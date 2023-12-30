315849aa7349b8fd4976525e883b18f920bed3bf
Daniel Bárbara <daniel.santos.barbara@gmail.com>
2023-12-30 21:12:48 +0000

    fix design leaderboard

--- eagles-on-the-road/components/leaderCard.tsx
+++ eagles-on-the-road/components/leaderCard.tsx
@@ -10,11 +10,13 @@ export function LeaderCard({data, showFields}: LProps){
         <div className="flex flex-col items-center gap-4 text-lg"> 
            {field && data && data[field]?.map((el: any, i:number) => 
             <div
-            className={`flex justify-around ${i === 0 ? 'bg-orange-200' : i === 1 ? 'bg-gray-300' : i === 2 ? 'bg-orange-400' : 'bg-white'}
-            h-16 w-11/12 rounded-lg`} 
+            className={`flex justify-between ${i === 0 ? 'bg-orange-200' : i === 1 ? 'bg-gray-300' : i === 2 ? 'bg-orange-400' : 'bg-white'}
+            h-16 w-11/12 rounded-lg p-2`} 
             key={i}>
-            <p className="text-[1.8rem] self-center">{i === 0 ? '🥇' : i === 1 ? '🥈' : i === 2 ? '🥉' : ''}</p>
-            <img src={`/images/${data?.resutlPratice[0].photo}`} className="self-center h-[50px] w-[50px] rounded-[50%]"/>
+            <div className="flex gap-3 justify-self-start">
+                <p className="text-[1.8rem] self-center">{i === 0 ? '🥇' : i === 1 ? '🥈' : i === 2 ? '🥉' : ''}</p>
+                <img src={`/images/${data?.resutlPratice[0].photo}`} className="self-center h-[50px] w-[50px] rounded-[50%]"/>
+            </div>
             <p className="self-center">{el.name}</p>
             <p className="self-end">{el.distance ? el.distance : 0} kms</p>
             </div>
