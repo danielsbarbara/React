b82dd0c8287d535699e689741073445f887b6114
Daniel Bárbara <daniel.santos.barbara@gmail.com>
2024-03-11 12:43:09 +0000

    fixing error

--- eagles-on-the-road/pages/api/v1/users/[info].ts
+++ eagles-on-the-road/pages/api/v1/users/[info].ts
@@ -5,8 +5,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async (req:NextApiRequest, res:NextApiResponse) => {
     const userId = req.query.info
     const userInfo = await GetUserInfo(userId)
-    const kmPratice = await GetRunsKms(userId, 'practice')
-    const kmRuns = await GetRunsKms(userId, 'runs')
+    const kmPratice = await GetRunsKms(userId, 'practice', 'All')
+    const kmRuns = await GetRunsKms(userId, 'runs', 'All')
     userInfo.practice = kmPratice
     userInfo.runs = kmRuns
     res.status(200).json({result: userInfo})
