335d60d4730dcff54b431b485017ea06869bbec7
Daniel Bárbara <daniel.santos.barbara@gmail.com>
2024-03-11 13:05:02 +0000

    fixing errors

--- eagles-on-the-road/logic/frontend/leaderboard.ts
+++ eagles-on-the-road/logic/frontend/leaderboard.ts
@@ -4,7 +4,7 @@ export async function fetchLeaderBoard(value: string) {
         headers: {'Content-type': 'aplication/json'}
     }
 
-    const res = await fetch(`/api/v1/${value ? value : 'All'}`)
+    const res = await fetch(`/api/v1/${value ? value : 'All'}`, options)
     if(res.status === 200){
         const body = await res.json()
         return body.result
