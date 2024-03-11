d4e79c2fa125e260452c2945e667dbd9fa6b825e
Daniel Bárbara <daniel.santos.barbara@gmail.com>
2024-03-11 13:33:15 +0000

    fixing errors

--- eagles-on-the-road/pages/api/v1/leaderboard.ts
+++ eagles-on-the-road/pages/api/v1/leaderboard.ts
@@ -3,10 +3,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async(req: NextApiRequest, res: NextApiResponse) => {
     const leaderboardDate = req.body
-    if (typeof leaderboardDate === 'string'){
-        const date = leaderboardDate
-        const leaderboard = await getLeaderBoard(date)
-        res.status(200).json({result: leaderboard})
+    if(req.method === 'POST'){
+        if (typeof leaderboardDate === 'string'){
+            const date = leaderboardDate
+            const leaderboard = await getLeaderBoard(date)
+            res.status(200).json({result: leaderboard})
+        }
+            res.status(400).json({result: 'error'})
     }
-        res.status(400).json({result: 'error'})
 }
